efe1ffb62ee10016c1e6a037ffb564d6473d1232
Pankaj Kumar Rajput <pankaj476987@gmail.com>
2025-10-17 21:56:49 +0530

    Time: 20 ms (7.06%), Space: 8.1 MB (45.24%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.c
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -5,11 +5,13 @@ bool isPalindrome(int x) {
     int org = x;
     long long rev = 0;
 
+
     while (x > 0) {
         int ld = x % 10;
         rev = rev * 10 + ld;
         x /= 10;
     }
+
 
     if (rev == org) {
         printf("true");
@@ -19,5 +21,4 @@ bool isPalindrome(int x) {
         return false;
       }
       
-   
 }
