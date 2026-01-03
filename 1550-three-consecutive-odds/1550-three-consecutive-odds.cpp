4f49f2f6e97c7316dfa31f6b6915f245badd2f4e
Pankaj Kumar Rajput <pankaj476987@gmail.com>
2026-01-03 17:49:13 +0530

    Time: 0 ms (100%), Space: 11.6 MB (99.29%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -5,12 +5,10 @@ public:
       for(int i: arr){
           if(i%2!=0){
               count++;
-            }  else{
-                  count = 0;
+              if(count==3) return true;
+            }else{
+                 count = 0;
                 }
-           if(count==3){
-               return true;
-            } 
         }return false;
     }    
 };
