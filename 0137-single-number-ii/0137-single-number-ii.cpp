e9b1667da6ae991e8abfe873e07e8ce6eef561b3
Pankaj Kumar Rajput <pankaj476987@gmail.com>
2026-01-12 22:30:19 +0530

    Time: 1 ms (41.57%), Space: 13.8 MB (23.58%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+      unordered_map < int,int > mp;
+      for(int i : nums) {
+          mp[i]++;
+        } 
+        for(auto i: mp) {
+          if(i.second != 3)
+          return i.first;
+        }
+        return 0;
+    }
+};
