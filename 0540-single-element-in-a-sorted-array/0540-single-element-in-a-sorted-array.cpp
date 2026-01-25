234ce5187368d69a8b7d28e19b65f84164ca8e13
Pankaj Kumar Rajput <pankaj476987@gmail.com>
2026-01-25 13:09:29 +0530

    Time: 28 ms (3.36%), Space: 31.9 MB (6.34%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+      map < int,int > mp;
+      int temp = 0;
+      for(int i : nums) {
+          mp[i]++;
+        }
+        for(auto i: mp) {
+          if(i.second==1) 
+          temp = i.first;
+        }
+        return temp;   
+    }
+};
