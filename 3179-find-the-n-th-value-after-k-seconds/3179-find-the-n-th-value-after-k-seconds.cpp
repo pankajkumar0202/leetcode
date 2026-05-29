0c30b15f5922954051a70b9a40129c05f0ffa9b1
Pankaj Kumar Rajput <pankaj476987@gmail.com>
2026-05-29 15:26:56 +0530

    Time: 209 ms (68.44%), Space: 11.4 MB (12.93%) - LeetHub

--- 3179-find-the-n-th-value-after-k-seconds/3179-find-the-n-th-value-after-k-seconds.cpp
+++ 3179-find-the-n-th-value-after-k-seconds/3179-find-the-n-th-value-after-k-seconds.cpp
@@ -3,7 +3,7 @@ public:
     int valueAfterKSeconds(int n, int k) {
         int i = 0;
         int count = 1;
-        const int temp1 = 1e9 + 7;
+        const int temp = 1e9 + 7;
         vector<int>ans(n);
         vector<int>pref(n);
         while(i != n) {
@@ -15,10 +15,8 @@ public:
            pref[i] = pref[i-1] + ans[i];
         }
         while(count != k) {
-           int temp = 0;
-           for(int i = 0; i<pref.size(); i++) {
-              pref[i] = (temp + pref[i]) % temp1;
-              temp = pref[i];
+           for(int i = 1; i<pref.size(); i++) {
+              pref[i] = (pref[i-1] + pref[i]) % temp;
            }
            count++;
         }
