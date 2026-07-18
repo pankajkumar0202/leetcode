76265fefe88385381bb86c733fcd00d8951b05ae
Pankaj Kumar Rajput <pankaj476987@gmail.com>
2026-07-18 14:07:15 +0530

    Time: 0 ms (100%), Space: 16.1 MB (92.83%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
@@ -4,17 +4,9 @@ public:
       int large = INT_MIN;
       int small = INT_MAX;
       for(int i = 0; i<nums.size(); i++){
-        if(large<nums[i]){
-             large = nums[i];
-           }  if(small>nums[i]){
-                 small = nums[i];
-               }
+          if(large < nums[i])  large = nums[i];
+          if(small > nums[i])  small = nums[i];
         }
-      while(large > 0 && small > 0){
-          if(large > small) large = large % small;
-          else small = small % large;
-        }
-          if(large==0) return small;
-          else return large; 
+        return gcd(large, small);
     }
 };
