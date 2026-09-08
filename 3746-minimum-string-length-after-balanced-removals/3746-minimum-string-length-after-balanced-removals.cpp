3bc9d43ec93f7ec2db57278a9ea7e03cddad2ab2
Pankaj Kumar Rajput <pankaj476987@gmail.com>
2026-09-08 22:48:11 +0530

    Time: 1 ms (79.11%), Space: 14.9 MB (45.39%) - LeetHub

---
+++ 3746-minimum-string-length-after-balanced-removals/3746-minimum-string-length-after-balanced-removals.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minLengthAfterRemovals(string s) {
+        int a = 0;
+        int b = 0;
+        for(int i = 0; i<s.size(); i++) {
+            if(s[i] == 'a') a++;
+            else b++;
+        }
+        return abs(a-b);
+    }
+};
