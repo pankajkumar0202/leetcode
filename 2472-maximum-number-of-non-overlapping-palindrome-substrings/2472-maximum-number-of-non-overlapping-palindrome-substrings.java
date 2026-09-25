fb29741a6669b1e234cd8c4f42e6e9cf64f6cd4d
Pankaj Kumar Rajput <pankaj476987@gmail.com>
2026-09-25 11:47:42 +0530

    Time: 2 ms (70.44%), Space: 44.1 MB (47.86%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -1,35 +1,29 @@
 class Solution {
-    boolean isPalindrome(String s,int i, int j) {
-
+    boolean isPalindrome(String s, int i, int j) {
         while(i < j) {
-            if(s.charAt(i) != s.charAt(j))  return false;
+            if(s.charAt(i) != s.charAt(j)) return false;
             i++;
             j--;
         }
         return true;
     }
-
     public int maxPalindromes(String s, int k) {
-        int count = 0;
-
-        for (int i = 0; i <= s.length() - k; i++) {
-            String temp = s.substring(i, i + k);
-
-            if (isPalindrome(temp,0,temp.length()-1)) {
-                count++;
-                i += k - 1;
+        int c = 0;
+        for(int i = 0; i<=s.length()-k; i++) {
+            String res = s.substring(i,i+k);
+            if(isPalindrome(res, 0, res.length()-1)) {
+                c++;
+                i = i+k-1;
             }
-
-            else if (i + k < s.length()) {
-                temp = s.substring(i, i + k + 1);
-
-                if (isPalindrome(temp,0,temp.length()-1)) {
-                    count++;
-                    i += k;
+            else if(i+k < s.length()) {
+                res = s.substring(i,i+k+1);
+                if(isPalindrome(res, 0, res.length()-1)) {
+                   c++;
+                   i = i+k;
                 }
             }
         }
         
-        return count;
+        return c; 
     }
 }
